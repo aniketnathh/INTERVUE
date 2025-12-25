61343503cf1334e7aefbc3e0d74948dd403b1641
anikethnathh <nathaniket679@gmail.com>
2025-12-25 21:22:10 +0530

    Tools env files connected

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,37 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import path from "path";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
+
+const __dirname = path.resolve();
 
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "success from api" });
 });
+app.get("/books", (req, res) => {
+  res.status(200).json({ msg: "this is book" });
+});
 
-app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+//make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => {
+      console.log("Server is running on port:", ENV.PORT);
+    });
+  } catch (error) {
+    console.error("error starting server", error);
+  }
+};
+
+startServer();
