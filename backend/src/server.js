19242c4615b7a1b7ab04a5a48cb42026a91eba6b
Aniket's code <nathaniket679@gmail.com>
2025-12-31 01:54:16 +0530

    Modify CORS callback to return false for disallowed origins
    Update CORS policy to deny disallowed origins instead of throwing an error.

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,7 +25,7 @@ app.use(
   cors({
     origin: function (origin, callback) {
       if (!origin || allowedOrigins.includes(origin)) callback(null, true);
-      else callback(new Error("Not allowed by CORS"));
+      else callback(null, false);
     },
     credentials: true,
   })
