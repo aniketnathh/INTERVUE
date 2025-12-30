3b1fc152caba9c866ce41df086845b57d93c0b4f
Aniket's code <nathaniket679@gmail.com>
2025-12-31 01:59:08 +0530

    Refactor server.js for cleaner middleware setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,64 +1,49 @@
 import express from "express";
-import { ENV } from "./lib/env.js";
 import path from "path";
-import { connectDB } from "./lib/db.js";
 import cors from "cors";
 import { serve } from "inngest/express";
-import { inngest, functions } from "./lib/inngest.js";
 import { clerkMiddleware } from "@clerk/express";
+
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+import { inngest, functions } from "./lib/inngest.js";
+
 import chatRoutes from "./routes/chatRoutes.js";
 import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
 
-//middlewares
+const __dirname = path.resolve();
+
+// middleware
 app.use(express.json());
-
 // credentials:true meaning?? => server allows a browser to include cookies on request
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://intervue-orpin.vercel.app",
-  "https://intervue-atkjs6h5y-anikets-projects-e791da45.vercel.app"
-];
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) callback(null, true);
-      else callback(null, false);
-    },
-    credentials: true,
-  })
-);
-
-
-// this adds authentication to req object
-app.use(clerkMiddleware());
-
-//inngest endpoint
 app.use("/api/inngest", serve({ client: inngest, functions }));
-
-//get chat token api
 app.use("/api/chat", chatRoutes);
 app.use("/api/sessions", sessionRoutes);
 
-const __dirname = path.resolve();
-
-
-app.get("/", (req, res) => {
-  res.status(200).json({ msg: "Backend is running" });
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
 });
 
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(ENV.PORT, () => {
-      console.log("Server is running on port:", ENV.PORT);
-    });
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
   } catch (error) {
-    console.error("error starting server", error);
+    console.error("💥 Error starting the server", error);
   }
 };
 
