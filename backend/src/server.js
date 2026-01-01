0aea208fe3e91265ba15720acf01b546f896652c
anikethnathh <nathaniket679@gmail.com>
2026-01-01 23:55:45 +0530

    configuration files; update server setup for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import cors from "cors";
 import { serve } from "inngest/express";
 import { clerkMiddleware } from "@clerk/express";
@@ -12,43 +13,46 @@ import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
 
-// Middleware
-app.use(express.json());
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
-app.use(clerkMiddleware());
+const __dirname = path.resolve();
 
-// Inngest + API routes
+// middleware
+app.use(express.json());
+// credentials:true meaning?? => server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
+
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/chat", chatRoutes);
 app.use("/api/sessions", sessionRoutes);
 
-// Health check
 app.get("/health", (req, res) => {
-  res.status(200).json({ msg: "API is up and running" });
+  res.status(200).json({ msg: "api is up and running" });
 });
 
-/**
- * Local dev server
- * Only start server locally
- */
-if (process.env.NODE_ENV == "production") {
-  const startServer = async () => {
-    try {
-      await connectDB();
-      app.listen(ENV.PORT, () =>
-        console.log("Server is running on port:", ENV.PORT)
-      );
-    } catch (error) {
-      console.error("💥 Error starting the server", error);
-    }
-  };
-  startServer();
+// make our app ready for deployment
+if (process.env.NODE_ENV === "production") {
+  // Move up two levels: from /src to /backend, then to /root
+  const rootPath = path.resolve(__dirname, "../../");
+  const buildPath = path.join(rootPath, "frontend", "dist");
+
+  // 1. Serve static files from the frontend/dist folder
+  app.use(express.static(buildPath));
+
+  // 2. Handle SPA routing (must use "*" and no "any" syntax)
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(buildPath, "index.html"));
+  });
 }
 
-/**
- * Vercel serverless handler
- */
-export default async function handler(req, res) {
-  await connectDB(); // ensures DB is connected on each invocation
-  return app(req, res);
-}
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () =>
+      console.log("Server is running on port:", ENV.PORT)
+    );
+  } catch (error) {
+    console.error("💥 Error starting the server", error);
+  }
+};
+
+startServer();
